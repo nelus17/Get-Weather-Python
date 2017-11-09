b93f83a26aae9e87225bd44b77b70e78ab6dbba9
Stas <arhmachael@gmail.com>
2017-11-09 15:15:03 +0200

    Add files via upload

--- weather.py
+++ weather.py
@@ -2,12 +2,12 @@ import pyowm
 
 city = input("What city are you interested in? ")
 
-owm = pyowm.OWM("Your API key")
+owm = pyowm.OWM("563ed332058c2c4cf02ef452ed5a36f9")
 
 observation = owm.weather_at_place(city)
 w = observation.get_weather()
 temperature = w.get_temperature('celsius')['temp']
 
 print("In the city " + city + " now " + str(temperature) + " celsius")
-print("Also in the specified city " + w.get_detailed_status() + " and wind speed is " + str(w.get_wind()['speed']))
-print("Humidity in the " + city + " is " + str(w.get_humidity()))
+print("Also in the specified city " + w.get_detailed_status() + " and wind speed is " + str(w.get_wind()['speed']) + " m/s")
+print("Humidity is the " + city + " is " + str(w.get_humidity()))
